1993fc6e2a8dc8946f160ed2bad797f1b12c34a5
pcm0720 <pcm0720@gmail.com>
2022-02-03 23:46:22 +0900

    순열

--- src/codeplus/basic/bruteforce/permutation/No_10973.java
+++ src/codeplus/basic/bruteforce/permutation/No_10973.java
@@ -3,36 +3,38 @@ package codeplus.basic.bruteforce.permutation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class No_10972 {
-	
-	static int arr[];
-	static int N;
+public class No_10973 {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(br.readLine());
-
-		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 		
-		if(check(arr)) {
+		if(func(arr)) {
 			for(int i : arr) {
 				System.out.print(i + " ");
 			}System.out.println();
 		}
+			
+		
 		br.close();
 	}
 
-	private static boolean check(int[] arr) {
+	private static boolean func(int[] arr) {
 		int i = arr.length - 1;
-		while(i > 0 && arr[i-1] > arr[i]) i--;
 		
-		if(i==0) return false;
+		while(i>0 && arr[i] < arr[i-1]) i--;
+		if(i == 0) return false;
 		
 		int j = arr.length - 1;
+		
 		while(arr[j] <= arr[i-1]) j--;
 		
 		swap(arr, i-1, j);
@@ -43,10 +45,9 @@ public class No_10972 {
 			j--;
 		}
 		return true;
-		
 	}
 
-	private static void swap(int[] arr2, int i, int j) {
+	private static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
--- src/codeplus/basic/bruteforce_recursion/No1248.java
+++ src/codeplus/basic/bruteforce_recursion/No1248.java
@@ -5,50 +5,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class No1248 {
-	
+
 	static int N;
-	static int[] arr;
-	static char[][] signs;
-	
-	public static void main(String[] args) throws IOException {
+	static int arr[];
+	static char signs[][];
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N];
 		signs = new char[N][N];
-		
+
 		String line = br.readLine();
+
 		int idx = 0;
-		
-		for(int i=0; i<N; i++) {
-			for(int j=i; j<N; j++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j < N; j++) {
 				signs[i][j] = line.charAt(idx++);
 			}
 		}
-		
+
 		dfs(0);
 		br.close();
 	}
 
 	private static void dfs(int depth) {
-		if(depth == N) {
-			for(int i=0; i<N; i++)
-				System.out.print(arr[i] + " ");
+
+		if (depth == N) {
+			for (int i : arr) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
 			System.exit(0);
-			return;
 		}
-		
-		for(int i= -10; i<= 10; i++) {
+		for (int i = -10; i <= 10; i++) {
 			arr[depth] = i;
-			if(check(depth))dfs(depth + 1);
+			if (check(depth + 1))
+				dfs(depth + 1);
 		}
 	}
 
 	private static boolean check(int depth) {
-		for(int i=0; i<=depth; i++) {
+		for (int i = 0; i < depth; i++) {
 			int sum = 0;
-			for(int j=i; j<=depth; j++) {
+			for (int j = i; j < depth; j++) {
 				sum += arr[j];
-				if(signs[i][j] != (sum == 0 ? '0' :  (sum > 0 ? '+' : '-'))) {
+				if (signs[i][j] != (sum == 0 ? '0' : (sum < 0 ? '-' : '+'))) {
 					return false;
 				}
 			}
